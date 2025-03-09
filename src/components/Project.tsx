c82242d9145d997660b34a9e56a5767968ef1228
Zander Sparrow <alexsilverhawk@gmail.com>
2025-03-08 23:08:35 -0800

    Clean up assets

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import gol from '../assets/images/gol.png';
 import mock03 from '../assets/images/mock03.png';
-import mock04 from '../assets/images/dory.jpeg';
-import mock05 from '../assets/images/pavise.png';
-import mock06 from '../assets/images/recipes.png';
+import dory from '../assets/images/dory.jpeg';
+import pavise from '../assets/images/pavise.png';
+import recipe from '../assets/images/recipes.png';
 import '../assets/styles/Project.scss';
 
 function Project() {
@@ -17,12 +17,12 @@ function Project() {
                 <p>An implementation of Conway's Game of Life using Processing.</p>
             </div>
             <div className="project">
-                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={mock05} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><img src={pavise} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://www.byuh.edu/covid-19-case-management" target="_blank" rel="noreferrer"><h2>Pavise</h2></a>
                 <p>A text-based game written in Python</p>
             </div>
             <div className="project">
-                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={mock04} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><img src={dory} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="https://github.com/yujisatojr/multi-reg-analysis" target="_blank" rel="noreferrer"><h2>Connection Project</h2></a>
                 <p>An interactive art installation displayed at Gray Area as part of the code immersion exhibition.</p>
             </div>
@@ -32,7 +32,7 @@ function Project() {
                 <p>A React Native mobile app for Ask Darcel, a project for connecting unhoused people with resources.</p>
             </div>
             <div className="project">
-                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={mock06} className="zoom" alt="thumbnail" width="100%"/></a>
+                <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><img src={recipe} className="zoom" alt="thumbnail" width="100%"/></a>
                 <a href="http://www.wemanage.jp/" target="_blank" rel="noreferrer"><h2>Recipe Box</h2></a>
                 <p>An API for storing and sharing recipes, built in python with Django.</p>
             </div>
